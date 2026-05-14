184eb25abb1b7b02fc2a0a054ad00a61ffc1d021
Y <chunhui@hui.local>
2026-05-14 23:14:47 +0800

    constructing a cpu monitor linear graph

--- my_vue_project/frontend/src/App.vue
+++ my_vue_project/frontend/src/App.vue
@@ -1,27 +1,7 @@
 <script setup>
-import {ref, onMounted} from 'vue' 
-import axios from 'axios' //connect from front
-
-const cpu = ref(0)
-const memory = ref(0)
-
-const fetchdata = async() =>{
-  const res = await axios.get('http://127.0.0.1:8000/system')
-  cpu.value = res.data.cpu
-  memory.value = res.data.memory
-}
-onMounted(()=>
-  fetchdata()
-)
+import cupchart from './component/CupChart.vue'
 </script>
 
 <template>
-  <div style = "padding:20px">
-  <h1>Ai Monitor MVP</h1>
-  
-  <p>cpu: {{cpu}}</p>
-  <p>memory: {{memory}}</p>
-  
-  <button @click="fetchdata">refresh</button>
-  </div>
+  <cupchart/>
 </template>
